ae3ace4e0f69b97a392eaf82becc32c998420773
Tony Hirst <tony.hirst@gmail.com>
2020-08-12 10:23:01 +0100

    Update cli.py

--- cqcdata/cli.py
+++ cqcdata/cli.py
@@ -6,10 +6,10 @@ import sqlite3
 @click.option('--dbname', default='cqc_data.db',  help='SQLite database name')
 @click.argument('command')
 def cli(dbname,command):
-	click.echo('Using SQLite3 database: {}'.format(dbname))
-	if command == 'collect':
-		con = cqc.setdb(dbname)
-		cqc._getDatasetURLs() 
-		cqc._get_care_directory(con)
-		cqc._get_active_locations(con)
-		cqc._get_ratings(con)
+    click.echo('Using SQLite3 database: {}'.format(dbname))
+    if command == 'collect':
+        con = cqc.setdb(dbname)
+        cqc._getDatasetURLs() 
+        cqc._get_care_directory(con)
+        #cqc._get_active_locations(con)
+        #cqc._get_ratings(con)
